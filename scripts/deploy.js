e66423da37c78112336d759cb30239b661c4cb0e
HUZAIFA HATIM <90339379+huzaifahatim@users.noreply.github.com>
2022-08-30 00:44:06 +0500

    test

---
+++ scripts/deploy.js
@@ -0,0 +1,29 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    const weiAmount = (await deployer.getBalance()).toString();
+  
+    console.log("Account balance:", (await ethers.utils.formatEther(weiAmount)));
+  
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy();
+    await token.deployed()
+  
+    const NFTCOLLECTION = await ethers.getContractFactory("NFT");
+    const myNFT = await NFTCOLLECTION.deploy(token.address,"ipfs://ThisIsHiddenCID/","ipfs://ThisIsCID/")
+    await myNFT.deployed()
+  
+
+
+    console.log("Token address:", token.address);
+    console.log("Contract deployed to address:", myNFT.address)
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+  });
